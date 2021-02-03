31bf273aac5851617b2bea0551e8a19fdc9d5bfc
wyjeon <wyjeon.info@gmail.com>
2021-02-03 19:22:51 +0900

    #2.4 Conclusions

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+//action creator
 const addToDo = (text) => {
   return {
     type: ADD_TODO,
     text
   };
 };
-
 const deleteToDo = (id) => {
   return {
     type: DELETE_TODO,
@@ -21,24 +21,28 @@ const deleteToDo = (id) => {
   };
 };
 
+//reducer
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [{ text: action.text, id: Date.now() }, ...state];
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
     case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
     default:
       return state;
   }
 };
-const store = createStore(reducer);
 
+//store
+const store = createStore(reducer);
 store.subscribe(() => console.log(store.getState()));
 
+//dispatch()
 const dispatchAddToDo = (text) => {
   store.dispatch(addToDo(text));
 };
-
 const dispatchDeleteToDo = (e) => {
   const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id));
@@ -59,13 +63,14 @@ const paintToDos = () => {
   });
 };
 
+//subcribe()
 store.subscribe(paintToDos);
 
+//action
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
   dispatchAddToDo(toDo);
 };
-
 form.addEventListener("submit", onSubmit);
