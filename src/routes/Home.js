310f5720dc0ef688b9f34ffa5a48a5363b72cb5b
wyjeon <wyjeon.info@gmail.com>
2021-02-03 19:32:35 +0900

    #3.0 Setup

---
+++ src/routes/Home.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function Home() {
+  const [text, setText] = useState("");
+  function onChange(e) {
+    setText(e.target.value);
+  }
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+    </>
+  );
+}
+
+export default Home;
